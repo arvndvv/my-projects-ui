6c521fad5aba49265a833601c7c9a97db2d32b6a
sijo-neoito <sijo.v@neoito.com>
2022-03-22 11:59:09 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 className="text-green-600 text-3xl font-bold underline bg-red-500 py-10 text-center hover:bg-green-600 hover:text-red-500">
+      Hello world!
+    </h1>
     </div>
   );
 }
